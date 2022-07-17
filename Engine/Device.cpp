1e67d3a2ea281b2dffa858f042ee4a955b60776c
choisubin <chsbin99@gmail.com>
2022-07-17 22:01:58 +0900

    [Section1] 장치초기화1

---
+++ Engine/Device.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "Device.h"
+
+void Device::Init()
+{
+
+}
--- Engine/EnginePch.cpp
+++ Engine/EnginePch.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "EnginePch.h"
+#include "Engine.h"
+unique_ptr<Engine> GEngine;
 
 void HelloEngine()
 {
